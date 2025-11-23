c912638e81f14df5a8b6bb8543e68bbc7b44fb25
Rebeca Poma <mrebecapoma.6@gmail.com>
2025-11-23 23:55:20 +0100

    ✅cargamos las apis

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 
+const URL_PROVINCIAS = "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/Listados/Provincias/";
+const URL_MUNICIPIOS = "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/Listados/MunicipiosPorProvincia/";
+const URL_GASOLINERAS_PROVINCIA = "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroProvincia/";
+const URL_GASOLINERAS_MUNICIPIO = "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroMunicipio/";
+
+
+
+const selectProvincia = document.getElementById("provincia");
+const selectMuni = document.getElementById("municipio");
+const selectCombustible = document.getElementById("combustible");
+const inputAbiertas = document.getElementById("soloAbiertas");
+const btnBuscar = document.getElementById("buscar");
+const containerResultados = document.getElementById("resultados");
+
+
+
+
 
 
