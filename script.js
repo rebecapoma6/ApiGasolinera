9cdcfb9c08205375ed05b3995adf8950e70afd05
Rebeca Poma <mrebecapoma.6@gmail.com>
2025-11-24 00:36:32 +0100

    ✅carga de provincias y muni

--- script.js
+++ script.js
@@ -13,8 +13,51 @@ const inputAbiertas = document.getElementById("soloAbiertas");
 const btnBuscar = document.getElementById("buscar");
 const containerResultados = document.getElementById("resultados");
 
+// Cargar provincias al iniciar
+async function cargarProvincias() {
+    try {
+        const respuesta = await fetch(URL_PROVINCIAS);
+        const datos = await respuesta.json();
+        console.log(datos);
 
 
+        datos.forEach(provincia => {
+            const opcion = document.createElement("option");
+            opcion.value = provincia.IDPovincia;
+            opcion.textContent = provincia.Provincia;
+            selectProvincia.appendChild(opcion);
+        });
+    } catch (error) {
+        console.error("Error al cargar provincias:", error);
+    }
+}
 
 
+async function cargarMunicipios(idProvincia) {
+    selectMuni.innerHTML = '<option value="">Seleccione Municipio</option>';
+    selectMuni.disabled = true;
+    if (!idProvincia) return;
+    try {
+        const respuesta = await fetch(`${URL_MUNICIPIOS}${idProvincia}`);
+        const datos = await respuesta.json();
+        console.log(datos);
 
+        datos.forEach(municipio => {
+            const opcion = document.createElement("option");
+            opcion.value = municipio.IDMunicipio;
+            opcion.textContent = municipio.Municipio;
+            selectMuni.appendChild(opcion);
+        });
+        selectMuni.disabled = false;
+    } catch (error) {
+        console.error("Error al cargar municipios:", error);
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    cargarProvincias();
+    selectProvincia.addEventListener("change", (e) => {
+        cargarMunicipios(e.target.value);
+    });
+});
